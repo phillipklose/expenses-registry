6225d686a8e725595f24f60216a40ef10ee10f49
phillipklose <filip.klose@gmail.com>
2021-12-27 23:32:10 +0100

    Initialized Kotlin spring/web project for basic expenses registry.

---
+++ src/main/kotlin/com/phillipklose/expensesregistry/ui/rest/dto/Expense.kt
@@ -0,0 +1,3 @@
+package com.phillipklose.expensesregistry.ui.rest.dto
+
+data class Expense(val id: String?, val amount: Double, val description: String)
---
+++ src/test/kotlin/com/phillipklose/expensesregistry/ExpensesRegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.phillipklose.expensesregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExpensesRegistryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
